c8482ce8287bd2b775673a5096de6edf99cabb68
ArathyLakshmi <arathy.l@gmail.com>
2017-05-22 21:26:38 -0400

    Add files via upload
    Sample solution built to practice easy LINQ coding

---
+++ SumOfMultiples/SumOfMultiples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfMultiples
+{
+    class Program
+    {
+        /// <summary>
+        /// Your task is to write function findSum.
+        /// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+        ///For example:
+        ///findSum(5) should return 8 (3 + 5)
+        ///findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the range");
+            int num = 0;
+            int.TryParse(Console.ReadLine(), out num);
+            int res = GetSum(num);
+            Console.WriteLine("result=" + res.ToString());
+            Console.ReadLine();
+        }
+
+        private static int GetSum(int n)
+
+        {
+
+            return Enumerable.Range(1, n)
+                                 .Where(m => m % 3 == 0 || m % 5 == 0).Sum();
+
+
+
+
+
+        }
+    }
+}
